f42ac00ac044aa4f5f8f17ac76c36009abb87e43
Aleksandr Didenko <adidenko@mirantis.com>
2014-04-11 10:17:02 +0300

    Separate docker build and run stages in manifests

--- site.pp
+++ site.pp
@@ -1,14 +1,14 @@
 $fuel_settings = parseyaml($astute_settings_yaml)
+$fuel_version = parseyaml($fuel_version_yaml)
+
+if is_hash($::fuel_version) and $::fuel_version['VERSION'] and $::fuel_version['VERSION']['production'] {
+  $production = $::fuel_version['VERSION']['production']
+}
+else {
+  $production = 'docker-build'
+}
 
 $postgres_default_version = '8.4'
-
-# postgresql server
-class { 'postgresql::server':
-  config_hash => {
-    'ip_mask_allow_all_users' => '0.0.0.0/0',
-    'listen_addresses'        => '0.0.0.0',
-  },
-}
 
 # nailgun db
 $database_name = "nailgun"
@@ -17,21 +17,30 @@ $database_port = "5432"
 $database_user = "nailgun"
 $database_passwd = "nailgun"
 
-class { "nailgun::database":
-  user      => $database_user,
-  password  => $database_passwd,
-  dbname    => $database_name,
-}
-
 # ostf db
 $dbuser   = 'ostf'
 $dbpass   = 'ostf'
 $dbname   = 'ostf'
 
-postgresql::db{ $dbname:
-  user     => $dbuser,
-  password => $dbpass,
-  grant    => 'all',
-  require => Class['::postgresql::server'],
+if $production == "docker-build" {
+  # postgresql server
+  class { 'postgresql::server':
+    config_hash => {
+      'ip_mask_allow_all_users' => '0.0.0.0/0',
+      'listen_addresses'        => '0.0.0.0',
+    },
+  }
+  
+  class { "nailgun::database":
+    user      => $database_user,
+    password  => $database_passwd,
+    dbname    => $database_name,
+  }
+  
+  postgresql::db{ $dbname:
+    user     => $dbuser,
+    password => $dbpass,
+    grant    => 'all',
+    require => Class['::postgresql::server'],
+  }
 }
-
